5164c6824d3f562cfb238ee7294808dd4ab729a4
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-02-19 10:17:15 -0500

    added User Schema and linked to connections

--- models/userModels.js
+++ models/userModels.js
@@ -2,5 +2,11 @@ const mongoose = require("../db/connection");
 const Schema = mongoose.Schema;
 
 const User = new Schema({
-  content: String
+  email: String,
+  password: String,
+  recipes: {
+    type: Schema.Types.ObjectId,
+    ref: "Recipe"
+  }
 });
+module.exports = mongoose.model("User", User);
