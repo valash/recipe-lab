5c16b963b32a6c728c2d84ac6b9d20b758b22603
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-02-19 16:07:53 -0500

    user route and adding mongoose

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 //this is for authentication!!! (Bonus 1)
 // const passport = require("passport");
 
+app.use(require("./routes/userRoute"));
 app.listen(3000, () => {
   console.log("listening on THE PORT YALL");
 });
