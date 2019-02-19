0eaed32b880fd347d45394f86ee9dc4554d6cbca
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-02-19 09:34:40 -0500

    recipe Schema

---
+++ models/recipesModels.js
@@ -0,0 +1,11 @@
+const Recipe = new Schema = ({
+    content: String,
+    whenCreated: {
+        default: Date.now()
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+
+})
