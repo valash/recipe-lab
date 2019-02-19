eb0609cd41bbbfde41d19d1cd17a5c5aca9c64d2
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-02-19 16:41:58 -0500

    just in case

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ const app = express();
 //this is for authentication!!! (Bonus 1)
 // const passport = require("passport");
 
-
 app.use(require("./routes/userRoute"));
-=======
-app.use(function (req, res) {
-    res.status(404).render('error');
-});
 
+app.use(function(req, res) {
+  res.status(404).render("error");
+});
 
 app.listen(3000, () => {
   console.log("listening on THE PORT YALL");
