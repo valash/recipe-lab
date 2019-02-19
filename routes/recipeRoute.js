601cd02ecb04087af9cb8c48e48cb637ab4f314c
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-02-19 15:06:52 -0500

    added route stuff

--- routes/recipeRoute.js
+++ routes/recipeRoute.js
@@ -1,3 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const routeController = require("../controllers/recipe");
+
+//CRUD: Create Read Update Delete
+//for recipes i want the user to be able to:
+//Create a recipe -->PUT
+//view a recipe, like on a feed -->GET
+//delete a recipe --> ?_methodOverride
